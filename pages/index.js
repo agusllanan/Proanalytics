ef327f0a9590535baf29e4dfa5e84639c5f20de8
agusllanan <agus.llanan77@gmail.com>
2021-08-26 21:59:55 -0300

    Ultima version de next js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
                 hot chicken authentic tumeric truffaut hexagon try-hard chambray
               </p>
               <div className="flex justify-center">
-                <button className="inline-flex text-white bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-400 rounded text-lg">
+                <button className="inline-flex text-white mx-auto bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-400 rounded text-lg">
                   Contactanos
                 </button>
               </div>
