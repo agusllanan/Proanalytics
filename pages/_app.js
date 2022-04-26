a3f43b87d7e7e38439b2ec666cbbec0adfd289d3
Agustín Llanán <71727653+agusllanan@users.noreply.github.com>
2022-04-25 22:23:59 -0300

    Google tag

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-C17VELZVCD`}
         />
         <Script strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            gtag('config', 'G-C17VELZVCD', {
             page_path: window.location.pathname,
             });
           `}
